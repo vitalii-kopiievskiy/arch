303a69a726738ce9be0d616d33d4cefed7152135
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2021-05-02 19:23:01 +0300

    Add NGrx models

---
+++ src/app/store/dictionaries/dictionaries.models.ts
@@ -0,0 +1,15 @@
+import {ControlItem, Item, Icon} from '../../models/frontend';
+export {ControlItem, Item } from '../../models/frontend';
+
+export interface Dictionaries {
+  roles: Dictionary;
+  specializations: Dictionary;
+  qualifications: Dictionary;
+  skills: Dictionary;
+}
+
+
+export interface Dictionary {
+  items: Item[];
+  controlsItems: ControlItem[];
+}
